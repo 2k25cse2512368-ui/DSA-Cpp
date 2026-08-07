95108570093b039e5a9f279780652d8b892b7e32
shreya_trivedi_000 <2k25cse2512368@gmail.com>
2026-08-07 22:27:15 +0530

    commit;

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  //basic input output header file
+using namespace std; // use to avoid the standard namespace
+int main()
+{
+    cout << "Hello World!" << endl
+         << "Myself, Shreya Trivedi"; // endl used to end the line
+    return 0;
+}
