81ea68f2168557db6356d84262b5f144fadf0e98
shreya_trivedi_000 <2k25cse2512368@gmail.com>
2026-08-09 20:35:24 +0530

    Boilerplate of C++

---
+++ 01_Fundamentals/03_Boilerplate_C++.cpp
@@ -0,0 +1,21 @@
+/* Extension : .cpp
+   Language : C++17
+   Case Sensitive : Yes
+
+*/                          // Multi-line comment  
+
+# include <iostream>         //Pre processor directive to include the basic input output header file
+
+using namespace std;        // use to avoid the standard namespace[ex-std::cout(before),cout<<(after)]
+
+int main(){                 //Sarting point of the program that is to be executed
+
+    cout<<"Here is the predefined boilerplate code for C++17"<<endl;
+
+    cout<<"Done!"<<endl;      //end of the line (enter) and move to the next line
+
+    cout<<"This is the end of the program"<<"\n";//same work time complexity of \n is less than endl 
+
+    return 0;              //Code executed successfully and it return 0 to the operating system
+
+}
