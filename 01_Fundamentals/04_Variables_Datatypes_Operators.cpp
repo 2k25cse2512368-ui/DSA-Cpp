14899db7e3c69ca4cce76088cd2d8634be90af9e
shreya_trivedi_000 <2k25cse2512368@gmail.com>
2026-08-10 21:34:21 +0530

    Variables Basis

---
+++ 01_Fundamentals/04_Variables_Datatypes_Operators.cpp
@@ -0,0 +1,47 @@
+/* VARIABLES IN C++
+
+Variables are containers used to store values/data.
+
+Common Data Types:
+    1. int
+    2. float
+    3. char
+    4. double
+    5. bool
+
+*/
+
+# include <iostream>
+using namespace std;
+int main() {
+
+    // =========================
+    // INTEGER (int)
+    // =========================
+
+    int a;
+    cout << "Enter an integer number: ";
+    cin >> a;
+    cout << "\nThe value of a is: " << a << endl;
+    cout << "Size of a: " << sizeof(a) << " bytes" << endl; // sizeof() tells the memory occupied by the variable in bytes
+
+
+    // =========================
+    // CHARACTER (char)
+    // =========================
+
+    char b;
+    cout << "\nEnter a character: ";
+    cin >> b;
+    cout << "The character is: " << b << endl;
+
+
+    // =========================
+    // TYPE CASTING
+    // =========================
+
+    cout << "ASCII value of b: " << (int)b << endl;    //American Standard Code for Information Interchange
+
+
+    return 0;
+}
